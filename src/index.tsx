0e6637d3072e74a6a24b401f0cfd4fa8b53940e0
John Rees <john@bitsushi.com>
2018-11-26 12:00:00 +0000

    change material on hover

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { inject, observer, Provider } from "mobx-react";
 import * as React from "react";
 import { render } from "react-dom";
 import { compose } from "recompose";
+import { fromEvent } from "rxjs";
+import { scan } from "rxjs/operators";
 import * as THREE from "three";
 import Project from "./stores/project";
 
@@ -47,23 +49,73 @@ const Info = compose(
   );
 });
 
+class Hanger {
+  mesh: THREE.Mesh;
+  geometry = new THREE.BoxGeometry(1, 1, 1);
+  material = new THREE.MeshNormalMaterial({ opacity: 0.5, transparent: true });
+
+  constructor(private editor) {
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+  }
+
+  handleMouseOver(intersection) {
+    this.material.opacity = 1;
+    requestAnimationFrame(this.editor.animate);
+    console.log(intersection);
+  }
+  handleMouseOut() {
+    this.material.opacity = 0.5;
+    requestAnimationFrame(this.editor.animate);
+    console.log("out");
+  }
+}
+
 class Editor extends React.Component {
   private camera;
   private container: HTMLDivElement;
-  // private objectsToRaycast: THREE.Object3D[];
   // private orbitControls: THREE.OrbitControls;
-  // private raycaster = new THREE.Raycaster();
+  // private objectsToRaycast: THREE.Object3D[];
+  private raycaster = new THREE.Raycaster();
   private renderer = new THREE.WebGLRenderer({ antialias: true });
   private scene = new THREE.Scene();
+  private hanger;
+  private intersections = [];
 
   componentDidMount() {
+    this.hanger = new Hanger(this);
+
     this.renderer.setClearColor(0xeeeeee);
 
     this.camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-    this.camera.position.set(-30, 100, 70);
+    this.camera.position.set(-3, 7, 5);
     this.camera.lookAt(0, 0, 0);
 
     this.container.appendChild(this.renderer.domElement);
+
+    this.scene.add(this.hanger.mesh);
+
+    const mouseMove$ = fromEvent(this.renderer.domElement, "mousemove");
+
+    mouseMove$
+      .pipe(
+        scan(
+          (acc, event: MouseEvent): any => {
+            acc.x = (event.layerX / 500) * 2 - 1;
+            acc.y = (event.layerY / 500) * 2 - 1;
+            return acc;
+          },
+          { x: 0, y: 0 }
+        )
+      )
+      .subscribe(xy => {
+        this.raycaster.setFromCamera(xy, this.camera);
+        this.intersections = this.raycaster.intersectObject(this.hanger.mesh);
+        if (this.intersections.length > 0) {
+          this.hanger.handleMouseOver(this.intersections[0]);
+        } else {
+          this.hanger.handleMouseOut();
+        }
+      });
 
     this.handleResize();
   }
